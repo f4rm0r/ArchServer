0b46ee147fc39d7eb06315ee30f11a55ca2250b6
f4rm0r <anders.buja@gmail.com>
2021-10-21 01:21:25 +0200

    Update 1-setup.sh

--- 1-setup.sh
+++ 1-setup.sh
@@ -1,1 +1,42 @@
 #!/usr/bin/env bash
+
+echo "--------------------------------------"
+echo "--           Network Setup          --"
+echo "--------------------------------------"
+pacman -S networkmanager dhclient --noconfirm --needed
+systemctl enable --now NetworkManager
+
+echo "--------------------------------------"
+echo "--      Set Password for Root       --"
+echo "--------------------------------------"
+echo "Enter password for root: "
+passwd root
+
+if ! source install.conf; then
+    read -p "Please enter hostname:" hostname
+
+    read -p "Please enter username:" username
+    echo "username=$username" >> /ArchServer/install.conf
+    echo "password=$password" >> /ArchServer/install.conf
+fi
+
+echo "-----------------------------------------"
+echo "----       Setting makeflags         ----"
+echo "-----------------------------------------"
+
+nc=$(grep -c ^processor /proc/cpuinfo)
+echo "You have " $nc" cores."
+echo "-----------------------------------------"
+echo "Changing the makeflags for "$nc" cores."
+sudo sed -i 's /#MAKEFLAGS="j2"/MAKEFLAGS="j$nc"/g' /etc/makepkg.conf
+echo "Changing the compression settings for "$nc" cores."
+sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T $nc -z -)/g' /etc/makepkg.conf
+
+echo "-------------------------------------------------"
+echo "       Setup Language to US and set locale       "
+echo "-------------------------------------------------"
+sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
+timedatectl --no-ask-password set-timezone Europe/Stockholm
+timedatectl --no-ask-password set-ntp 1
+localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_COLLATE="" LC_TIME="en_US.UTF-8"
