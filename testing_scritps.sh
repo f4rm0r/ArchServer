510c0db0e63c2ae0a8eb4a92c5df73ffe9a68370
f4rm0r <anders.buja@gmail.com>
2021-10-21 13:58:22 +0200

    Update testing_scritps.sh

--- testing_scritps.sh
+++ testing_scritps.sh
@@ -2,3 +2,18 @@
 read -p "Please enter username:" username
 
 echo -e "$username"
+
+useradd -m -g users -G wheel -s /bin/bash $username
+cp -R ~/ArchServer /home/$username/
+
+    echo "--------------------------------------"
+    echo "--      Set Password for $username  --"
+    echo "--------------------------------------"
+    echo "Enter password for $username user: "
+    passwd $username
+
+    cp /etc/skel/.bash_profile /home/$username/
+    cp /etc/skel/.bash_logout /home/$username/
+    cp /etc/skel/.bashrc /home/$username/.bashrc
+    chown -R $username: /home/$username
+    sed -n '#/home/'"$username"'/#,s#bash#zsh#' /etc/passwd
