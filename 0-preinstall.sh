51b82f700bd8b22b4891fcf3e3d6f950db87abeb
f4rm0r <anders.buja@gmail.com>
2021-10-21 14:03:30 +0200

    Update 0-preinstall.sh

--- 0-preinstall.sh
+++ 0-preinstall.sh
@@ -106,22 +106,21 @@ cp /etc/pacman.d/mirrorlist ${MOUNTPOINT}/etc/pacman.d/mirrorlist
 
 read -p "Please enter username:" username
 
+useradd -m -g users -G wheel -s /bin/bash $username
+cp -R ~/ArchServer /home/$username/
 
-    [ ! -d "/home/$username" ] && useradd -m -g users -G wheel -s /bin/bash $username 
-    cp -R ~/ArchServer /home/$username/
     echo "--------------------------------------"
     echo "--      Set Password for $username  --"
     echo "--------------------------------------"
     echo "Enter password for $username user: "
     passwd $username
+
     cp /etc/skel/.bash_profile /home/$username/
     cp /etc/skel/.bash_logout /home/$username/
     cp /etc/skel/.bashrc /home/$username/.bashrc
     chown -R $username: /home/$username
     sed -n '#/home/'"$username"'/#,s#bash#zsh#' /etc/passwd
-else
-	echo "You are already a user proceeding with aur installs"
-fi
+
 
 echo "--------------------------------------------------"
 echo "----------   System ready for 1-setup   ----------"
