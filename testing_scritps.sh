5648e1fd46ec645531fd9cea1cbab38193624389
Arch <anders.buja@gmail.com>
2022-09-21 01:17:23 +0200

    Update testing_scritps.sh

--- testing_scritps.sh
+++ testing_scritps.sh
@@ -1,8 +1,3 @@
-lsblk
-echo "Please enter disk to work on: (example /dev/sda)"
-read DISK
-echo "THIS WILL FORMAT AND DELETE ALL DATA ON THE DISK!"
-read -p "are you sure you want to continue (Y/N):" formatdisk
-case $formatdisk in
-
-y|Y|yes|Yes|YES)
+cat << EOF | sudo chroot chroot
+ls /
+EOF
