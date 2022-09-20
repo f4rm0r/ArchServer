6c7f794319bcf365841f02d541baa2c0b312cef4
Arch <anders.buja@gmail.com>
2022-09-21 00:56:06 +0200

    Update 0-preinstall.sh

--- 0-preinstall.sh
+++ 0-preinstall.sh
@@ -96,24 +96,6 @@ echo "--------------------------------------------------"
 cp /etc/pacman.d/mirrorlist ${MOUNTPOINT}/etc/pacman.d/mirrorlist
 
 
-read -p "Please enter username:" username
-
-useradd -m -g users -G wheel -s /bin/bash $username
-cp -R ~/ArchServer /home/$username/
-
-    echo "--------------------------------------"
-    echo "--      Set Password for $username  --"
-    echo "--------------------------------------"
-    echo "Enter password for $username user: "
-    passwd $username
-
-    cp /etc/skel/.bash_profile /home/$username/
-    cp /etc/skel/.bash_logout /home/$username/
-    cp /etc/skel/.bashrc /home/$username/.bashrc
-    chown -R $username: /home/$username
-#    sed -n '#/home/'"$username"'/#,s#bash#zsh#' /etc/passwd
-
-
 echo "--------------------------------------------------"
 echo "----------   System ready for 1-setup   ----------"
 echo "--------------------------------------------------"
