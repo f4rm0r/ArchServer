8bee67ee0be7ce9d4e1b5d874e973ca332c98688
Arch <anders.buja@gmail.com>
2022-09-21 22:35:46 +0200

    Update 0-preinstall.sh

--- 0-preinstall.sh
+++ 0-preinstall.sh
@@ -135,14 +135,12 @@ echo "--------------------------------------------------"
 echo "--------   Bootloader GRUB Installation  ---------"
 echo "--------------------------------------------------"
 
-grub-install --target=x86_64-efi --efi-directory=${MOUNTPOINT}/boot/efi --bootloader-id=ArchServer
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ArchServer
 
 echo "---------------------------------------"
 echo "------  setting up user account  ------"
 echo "---------------------------------------"
 useradd -m -g users -G wheel -s /bin/bash $username
-cp -R ~/ArchServer /home/$username/
-
 echo -e "$password\n$password" | passwd $username
 
 echo "--------------------------------------"
@@ -150,7 +148,6 @@ echo "--           Network Setup          --"
 echo "--------------------------------------"
 pacman -S networkmanager dhclient --noconfirm --needed
 systemctl enable --now NetworkManager
-
 
 cp /etc/skel/.bash_profile /home/$username/
 cp /etc/skel/.bash_logout /home/$username/
@@ -162,6 +159,85 @@ echo "--------------------------------------"
 echo "--    Setting Password for Root     --"
 echo "--------------------------------------"
 echo -e "$rootpw\n$rootpw" | passwd root
+
+
+echo "-----------------------------------------"
+echo "----       Setting makeflags         ----"
+echo "-----------------------------------------"
+
+nc=$(grep -c ^processor /proc/cpuinfo)
+echo "You have " $nc" cores."
+echo "-----------------------------------------"
+echo "Changing the makeflags for "$nc" cores."
+sudo sed -i 's/#MAKEFLAGS="j2"/MAKEFLAGS="j$nc"/g' /etc/makepkg.conf
+echo "Changing the compression settings for "$nc" cores."
+sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T $nc -z -)/g' /etc/makepkg.conf
+
+# Set keymaps
+localectl --no-ask-password set-keymap sv-latin1
+
+# Hostname
+hostnamectl --no-ask-password set-hostname $hostname
+
+#Add parallel downloading
+sed -i 's/^#Para/Para/' /etc/pacman.conf
+
+cat <<EOF >> /etc/pacman.conf
+[multilib]
+Include = /etc/pacman.d/mirrorlist
+EOF
+pacman -Syy --noconfirm
+
+echo -e "\nInstalling Base System\n"
+
+PKGS=(
+    'autoconf'
+    'automake'
+    'base'
+    'biunutils'
+    'btrfs-progs'
+    'dhcpcd'
+    'dialog'
+    'dosfstools'
+    'efibootmgr'
+    'gcc'
+    'git'
+    'htop'
+    'nethogs'
+    'ncdu'
+    'linux'
+    'linux-lts'
+    'linux-firmware'
+    'linux-headers'
+    'neofetch'
+    'rsync'
+    'sudo'
+    'traceroute'
+    'ufw'
+)
+
+for PKG in "${PKGS[@]}"; do
+    echo "INSTALLING: ${PKG}"
+    sudo pacman -S "$PKG" --noconfirm --needed
+done
+
+#
+# determine processor type and install microcode
+# 
+proc_type=$(lscpu | awk '/Vendor ID:/ {print $3}')
+case "$proc_type" in
+	GenuineIntel)
+		print "Installing Intel microcode"
+		pacman -S --noconfirm intel-ucode
+		proc_ucode=intel-ucode.img
+		;;
+	AuthenticAMD)
+		print "Installing AMD microcode"
+		pacman -S --noconfirm amd-ucode
+		proc_ucode=amd-ucode.img
+		;;
+esac
+echo -e "\nDone!\n"
 
 EOF
 
