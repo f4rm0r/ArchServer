224f1d0378ae705ba1fec157a108c6556d1b17e8
Arch <anders.buja@gmail.com>
2022-10-11 09:47:43 +0200

    Update testing_scripts.sh

--- testing_scripts.sh
+++ testing_scripts.sh
@@ -1,3 +1,53 @@
-cat << EOF | sudo arch-chroot /mnt
-ls /
-EOF
+nc=$(grep -c ^processor /proc/cpuinfo)
+echo "You have " $nc" cores."
+echo "-----------------------------------------"
+echo "Changing the makeflags for "$nc" cores."
+sudo sed -i 's/#MAKEFLAGS="j2"/MAKEFLAGS="j$nc"/g' /etc/makepkg.conf
+echo "Changing the compression settings for "$nc" cores."
+sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T $nc -z -)/g' /etc/makepkg.conf
+
+# Set keymaps
+localectl --no-ask-password set-keymap sv-latin1
+
+# Hostname
+hostnamectl --no-ask-password set-hostname $hostname
+
+#Add parallel downloading
+sed -i 's/^#Para/Para/' /etc/pacman.conf
+
+# Activates multilib
+echo "[multilib]" >> /etc/pacman.conf
+echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
+
+pacman -Syy --noconfirm
+
+echo -e "\nInstalling Base System\n"
+PKGS=(
+    'autoconf'
+    'automake'
+    'base'
+    'biunutils'
+    'btrfs-progs'
+    'dhcpcd'
+    'dialog'
+    'dosfstools'
+    'efibootmgr'
+    'gcc'
+    'git'
+    'htop'
+    'nethogs'
+    'ncdu'
+    'linux'
+    'linux-lts'
+    'linux-firmware'
+    'linux-headers'
+    'neofetch'
+    'rsync'
+    'sudo'
+    'traceroute'
+    'ufw'
+)
+for PKG in "${PKGS[@]}"; do
+    echo "INSTALLING: ${PKG}"
+    sudo pacman -S "$PKG" --noconfirm --needed
+done
